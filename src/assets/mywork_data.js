dc43493fff4ec1053e357688efd92f66b0619459
gayrn2310 <gayu.nadar2310@gmail.com>
2025-06-09 20:53:56 +0530

    update data

--- src/assets/mywork_data.js
+++ src/assets/mywork_data.js
@@ -1,9 +1,5 @@
 import project1_img from '../assets/project_1.png'
 import project2_img from '../assets/project_2.svg'
-import project3_img from '../assets/project_3.svg'
-import project4_img from '../assets/project_4.svg'
-import project5_img from '../assets/project_5.svg'
-import project6_img from '../assets/project_6.svg'
 
 const mywork_data = [
     {

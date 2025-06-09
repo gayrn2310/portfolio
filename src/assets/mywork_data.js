6cbe52809c54cb8a537c52a3c595be6168dc5ef9
gayrn2310 <gayu.nadar2310@gmail.com>
2025-06-09 20:53:21 +0530

    remove data

--- src/assets/mywork_data.js
+++ src/assets/mywork_data.js
@@ -16,26 +16,6 @@ const mywork_data = [
         w_name:"Web design",
         w_img:project2_img
     },
-    {
-        w_no:3,
-        w_name:"Web design",
-        w_img:project3_img
-    },
-    {
-        w_no:4,
-        w_name:"Web design",
-        w_img:project4_img
-    },
-    {
-        w_no:5,
-        w_name:"Web design",
-        w_img:project5_img
-    },
-    {
-        w_no:6,
-        w_name:"Web design",
-        w_img:project6_img
-    },
 ]
  
 export default mywork_data;
